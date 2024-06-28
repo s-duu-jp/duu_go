db2de792809f2e5d72ba7edeb08e02483c942c06
s-duu-jp <s@duu.jp>
2024-06-28 21:35:42 +0900

    fix

--- backend/api/handlers/handlers.go
+++ backend/api/handlers/handlers.go
@@ -1,6 +1,20 @@
 package handlers
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
 type Handlers interface {
 	AuthenticationHandlers
 	SampleHandlers
+}
+
+type AuthenticationHandlers interface {
+	PostLogin(c *gin.Context)
+	PostRefreshToken(c *gin.Context)
+	PostLogout(c *gin.Context)
+}
+
+type SampleHandlers interface {
+	PostSample(c *gin.Context)
 }
--- backend/api/handlers/sample.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-type SampleHandlers interface {
-	PostSample(c *gin.Context)
-}
