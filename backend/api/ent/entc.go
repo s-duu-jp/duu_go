485ff2f7f0515d8618f7e126cbdf18b56841c3ad
s-duu-jp <s@duu.jp>
2024-06-30 22:58:03 +0900

    migrate修正

--- backend/api/ent/entc.go
+++ backend/api/ent/entc.go
@@ -21,7 +21,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("creating entgql extension: %v", err)
 	}
-	if err := entc.Generate("./schema", &gen.Config{}, entc.Extensions(entGqlEx)); err != nil {
+	if err := entc.Generate("./schema", &gen.Config{Features: []gen.Feature{gen.FeatureVersionedMigration}}, entc.Extensions(entGqlEx)); err != nil {
 		log.Fatalf("running ent codegen: %v", err)
 	}
 }
