cf5c4671082cbe3bad360e789d7f1a52178e004b
s-duu-jp <s@duu.jp>
2024-07-13 08:57:49 +0900

    hoge

--- backend/api/handlers/restapi/authentication.go
+++ backend/api/handlers/restapi/authentication.go
@@ -1,4 +1,4 @@
-package impl
+package restapi
 
 import (
 	openapi "api/controllers/restapi"
--- backend/api/main.go
+++ backend/api/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	sw "api/controllers/restapi"
-	"api/handlers/impl"
+	"api/handlers/restapi"
 	"log"
 )
 
 func main() {
 	// RESTful API
 	router := sw.NewRouter(sw.ApiHandleFunctions{
-		AuthenticationAPI: sw.NewAuthenticationAPI(impl.NewAuthenticationHandlers()),
+		AuthenticationAPI: sw.NewAuthenticationAPI(restapi.NewAuthenticationHandlers()),
 	})
 	log.Fatal(router.Run(":3000"))
 }
