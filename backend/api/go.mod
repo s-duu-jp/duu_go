0a8c072d1752395492f0a7a34844fea2a0267f9d
s-duu-jp <s@duu.jp>
2024-07-13 09:18:49 +0900

    websocket

--- backend/api/go.mod
+++ backend/api/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/google/uuid v1.3.0
+	github.com/gorilla/websocket v1.5.0
 	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.16
 )
--- backend/api/main.go
+++ backend/api/main.go
@@ -3,13 +3,25 @@ package main
 import (
 	sw "api/controllers/restapi"
 	"api/handlers/restapi"
+	"api/handlers/websocket"
 	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// RESTful API
+	// WebSocketのメッセージ処理をゴルーチンとして開始
+	go websocket.HandleMessages()
+
+	// ルーターにRESTful APIのルートを追加
 	router := sw.NewRouter(sw.ApiHandleFunctions{
 		AuthenticationAPI: sw.NewAuthenticationAPI(restapi.NewAuthenticationHandlers()),
 	})
+
+	// WebSocketのハンドラを追加
+	router.GET("/chat", gin.WrapH(http.HandlerFunc(websocket.HandleConnections)))
+
+	// サーバーの起動
 	log.Fatal(router.Run(":3000"))
 }
